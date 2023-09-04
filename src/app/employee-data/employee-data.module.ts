b6b1317e37f4bb39a4b5146fd64d9c3339e61230
koushik s r <koushiksr1999@gmail.com>
2023-09-05 02:43:02 +0530

    fist front end commit

---
+++ src/app/employee-data/employee-data.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { EmployeeDataRoutingModule } from './employee-data-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    EmployeeDataRoutingModule
+  ]
+})
+export class EmployeeDataModule { }
